7c21fc4e6c82d462158954414c2a36d4e4e13126
Igor Volkov <igor.volkov@gmail.com>
2022-09-01 15:37:05 -0400

    ref added

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -1,5 +1,6 @@
-from subprocess import Popen
+# adapted from https://github.com/binder-examples/bokeh
 
+from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
